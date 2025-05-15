0c36dbbaab5a03c2b4272551651f816c6b79ae27
neyaz89 <nm001814@gmail.com>
2025-05-15 19:14:03 +0530

    flask change port

--- app.py
+++ app.py
@@ -137,4 +137,5 @@ def compress_pdf():
 
   if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
+
